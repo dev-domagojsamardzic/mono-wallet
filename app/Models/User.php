339ca3dd6b7ef570585e364357848cfc053c3991
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2023-04-21 18:14:15 +0200

    Edited User->fillable && UserFactory

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,19 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
+        'username',
+        'firstname',
+        'lastname',
+        'email'
     ];
 
     /**
