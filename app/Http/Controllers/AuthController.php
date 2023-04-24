8d737eeb81eb62c699be5c20a1e1883a29e3495e
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2023-04-24 11:48:02 +0200

    Modified AuthController::login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
+    /**
+     * Simple login function (send only username)
+     *
+     * @param App\Http\Requests\UserLoginRequest $request
+     *
+     * @return Illuminate\Support\Http\JsonResponse
+     */
 
-    public function login(UserLoginRequest $request) {
+    public function login(UserLoginRequest $request): JsonResponse
+    {
 
+        // Retrieve user by username
         $user = User::where('username', $request->username)->first();
+
+        // Delete previous tokens
+        $user->tokens()->delete();
+
+        // Create token and retrieve it's plain text value
         $token = $user->createToken('api_token')->plainTextToken;
 
         return response()->json([
