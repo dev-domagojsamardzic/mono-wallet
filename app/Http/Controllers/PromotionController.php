e67a58cb5f6af621deefa36e25f2334d177e47d0
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2023-05-05 15:54:52 +0200

    Removed excess functions from PromotionController

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -43,14 +43,6 @@ class PromotionController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(PromotionPostRequest $request)
@@ -94,29 +86,4 @@ class PromotionController extends Controller
 
 
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
-
 }
